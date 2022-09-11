f143cadb6b4c90330c2e564a7aee1bc9976a1682
OLGON92 <ogon318@gmail.com>
2022-09-10 23:37:32 -0700

    add some UI logic.

--- js/scripts.js
+++ js/scripts.js
@@ -8,16 +8,40 @@ function createArray(input) {
     }
     return numberArray;
 }
+
 function roboResponse(numberArray) {
   let newArray = []
   numberArray.forEach(function(element) {
     if (element.includes("3")) {
       newArray.push("Won't you be my neighbor?");
-    } else if(element.includes("2")) {
+    } else if (element.includes("2")) {
       newArray.push("Boop!");
+    } else if (element.includes("1")) {
+      newArray.push("Beep!");
     } else {
       newArray.push(element);
     }
   });
   return newArray.join(", ");
 };
+
+
+//User Logic
+window.addEventListener("load", function() {
+	document.querySelector("form#input-form").addEventListener("submit", roboResponse);
+  output-div.setAttribute("class", "hidden")
+  
+  
+  event.preventDefault();
+});
+/*$(document).ready(function() {
+  $("#input-form").submit(function(event) {
+    const userNumber = $("#input-number").val();
+    $(".hidden").hide();
+    $("#output-span").text(roboResponse(createArray(userNumber)));
+    $("#output-span").fadeIn()
+
+    window.scrollTo(0,document.body.scrollHeight);
+    event.preventDefault();
+  });
+})
