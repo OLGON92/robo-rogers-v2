3f8d31ced494785668cbd3b15fcefa43618550e5
OLGON92 <ogon318@gmail.com>
2022-09-10 20:42:30 -0700

    conduct 2nd test on roboResponse.

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,9 @@ function createArray(input) {
     }
     return numberArray;
 }
+function roboResponse(numberArray) {
+  let newArray = []
+  numberArray.forEach(function(element) {
+    if (element.includes("3")) {
+      newArray.push("Won't you be my neighbor?");
+    } else if()
